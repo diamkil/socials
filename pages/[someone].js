372fc8f131250a17b67b2ee61ac3a883317c438c
diamkil <diamkill@gmail.com>
2021-04-03 14:36:14 -0400

    Done stuff that doesn't work

--- pages/[someone].js
+++ pages/[someone].js
@@ -1,8 +1,8 @@
 import Head from 'next/head'
 import styles from '../styles/someone.module.css'
-import { getPeopleData } from '../lib/socials'
+import { getPeopleData, socialLink } from '../lib/socials'
 
-export default function Home({ socialsData }) {
+export default function Home({ socialsData, snapLink }) {
   return (
     <div className={styles.container}>
       <div className={styles.sociallinks}>
@@ -15,19 +15,24 @@ export default function Home({ socialsData }) {
             src={socialsData.picture}
             height={100}
             width={100}
+            alt={socialsData.name + "'s Image"}
           />
           <h1 className={styles.userh1}>Félix Pratt</h1>
         </div>
+
+        <div dangerouslySetInnerHTML={{ __html: snapLink }}/>
       </div>
     </div>
   )
 }
 
-export async function getServerSideProps() {
-  const socialsData = await getPeopleData("felix")
+export async function getServerSideProps({ params }) {
+  const socialsData = await getPeopleData(params.someone)
+  const snapLink = socialLink("snap", "felix.pratt")
   return {
     props: {
-      socialsData
+      socialsData,
+      snapLink
     }
   }
 }
---
+++ people/felix.md
@@ -0,0 +1,11 @@
+---
+name: 'Félix Pratt'
+picture:  'https://graph.facebook.com/934009904005836/picture?height=200&width=200'
+snap:  'felix.pratt'
+discord:  '229225747133235200'
+insta:  'felix.pratt'
+facebook:  'felix.prattd'
+twitter:  'felixdiamkil'
+applemusic:  'felixpratt'
+spotify: 'none'
+---
