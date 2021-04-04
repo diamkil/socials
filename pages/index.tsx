8455389ac10813ee006a83b2b92a2305191e78a9
root <root@DESKTOP-0P8LUHL.localdomain>
2021-04-04 13:48:14 -0400

    Changed some stuff

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head'
 export default function Home() {
   return (
     <Head>
-      <meta httpEquiv="refresh" content="0; URL=/felix" />
+      <meta httpEquiv="refresh" content="0; URL=/felix.pratt" />
     </Head>
   )
 }
