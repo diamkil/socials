ae194a3c89a750f6c373b1e21992299a072f4dbc
diamkil <diamkill@gmail.com>
2021-04-16 09:48:44 -0400

    Update Docker-Build.sh

--- Docker-Build.sh
+++ Docker-Build.sh
@@ -1,6 +1,7 @@
+project=socials
 current_commit=$(git rev-parse --short HEAD)
-current_commit_tag=diamkil/socials:$current_commit
-latest_tag=diamkil/socials:latest
+current_commit_tag=diamkil/$project:$current_commit
+latest_tag=diamkil/$project:latest
 
 docker build . -t $current_commit_tag -t $latest_tag
 docker image push $current_commit_tag
